41da03359b7a532514f6b7186cedf34fa94ea01b
siddhi1820 <69350158+siddhi1820@users.noreply.github.com>
2022-08-01 11:56:33 +0530

    Add files via upload

---
+++ input.java
@@ -0,0 +1,12 @@
+//Take user input as integer and display it.
+import java.util.Scanner; 
+class input
+{
+public static void main(String[] args)
+  {
+  Scanner a=new Scanner(System.in);
+  System.out.println("Enter a number:");
+  int number=a.nextInt();
+  System.out.println("Number is:"+number);
+  }
+}
