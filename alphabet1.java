e9e393ff59ad2c3fe7041f91063f06050dfe28bb
siddhi1820 <69350158+siddhi1820@users.noreply.github.com>
2022-08-01 14:26:02 +0530

    Add files via upload

---
+++ alphabet1.java
@@ -0,0 +1,24 @@
+//Java program to display a to z alphabet.
+
+import java.util.Scanner;
+class display
+{
+public static void main(String[] args)
+  {
+     char c;
+     for(c='a';c<='z';c++)
+     {
+       System.out.print(c+" ");
+     }   
+  } 
+}
+
+/* 
+output
+C:\Users>javac alphabet1.java
+
+C:\Users>java display
+a b c d e f g h i j k l m n o p q r s t u v w x y z
+C:\Users>
+
+*/
