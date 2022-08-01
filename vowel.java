10ee97e216b9221ac68512a62d0b130b50d31696
siddhi1820 <69350158+siddhi1820@users.noreply.github.com>
2022-08-01 13:24:48 +0530

    Add files via upload

---
+++ vowel.java
@@ -0,0 +1,44 @@
+//Java program check alphabet is vowel or consonant.
+/*
+vowel=['a','e','i','o','u']
+consonant=[b,c,d,f,...z] except a,e,i,o,u
+*/
+
+import java.util.Scanner;
+class check
+{
+public static void main(String[] args)
+  {
+     Scanner sc=new Scanner(System.in);
+     System.out.println("Enter a chracter:");
+     char ch=sc.next().charAt(0);
+     
+     if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u')
+     {
+       System.out.println(ch+" is a vowel.");
+     }
+     else
+     {
+       System.out.println(ch+" is a consonant.");
+     }
+  } 
+}
+
+/* 
+output
+C:\Users\siddh>javac vowel.java
+
+C:\Users>java check
+Enter a chracter:
+a
+a is a vowel.
+-----------END--------------
+C:\Users>javac vowel.java
+
+C:\Users>java check
+Enter a chracter:
+c
+c is a consonant.
+
+C:\Users>
+*/
