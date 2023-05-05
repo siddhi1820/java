bf14880ba797f23b8adfb85f2d8493eb7b27c329
siddhi1820 <69350158+siddhi1820@users.noreply.github.com>
2023-05-05 11:12:06 +0530

    Update newfile.java

--- File Handling/newfile.java
+++ File Handling/newfile.java
@@ -1,1 +1,17 @@
-
+package pro1;
+import java.io.File;
+class sc{
+	public static void main(String[] args) {
+		//create a new file
+		try {
+      // we create new file 
+		File f=new File("myfile.txt");
+		System.out.println("File sucessfully created.");
+		}
+		catch(Exception e) {
+      //if file is already existed then exception is catch.
+			System.out.println("File already exsisted!");
+		}
+		
+	}
+}
