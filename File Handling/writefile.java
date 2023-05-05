1c46e1c8c037ca2c971b11cecafa2b8fd98ffb31
siddhi1820 <69350158+siddhi1820@users.noreply.github.com>
2023-05-05 11:18:43 +0530

    Update writefile.java

--- File Handling/writefile.java
+++ File Handling/writefile.java
@@ -1,1 +1,21 @@
+package pro1;
+import java.io.File;
+import java.io.FileWriter;
+class sc{
+	public static void main(String[] args) {
+		//create a new file
+		try {
+		FileWriter fw=new FileWriter("myfile.txt");//open file to write
+	
+			fw.write("hello welcome to my file...");//write a file 
+		
+		System.out.println("Sucessfully write a file");
+		fw.close();//File close
+		}
+		catch(Exception e) {
+			System.out.println("File is not found!");
+		}
+		
+	}
+}
 
