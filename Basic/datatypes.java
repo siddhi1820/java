4fafcec23234bca23e58100839135fc238cd56b7
siddhi1820 <69350158+siddhi1820@users.noreply.github.com>
2023-09-02 13:04:33 +0530

    Create datatypes.java

---
+++ Basic/datatypes.java
@@ -0,0 +1,37 @@
+/*
+Datatypes in java:=
+byte : 8 bit (1 byte)
+short :16 bit (2 bytes)
+int :32 bit (4 bytes)
+float:32 bit(4 bytes)
+long:64 bit(8 bytes)
+double:64 bit (8 bytes)
+char:16bit (2 bytes)
+boolean:1 bit
+
+*/
+package basic;
+
+public class datatypes {
+
+	public static void main(String[] args) {
+		byte num=127;
+		int num1=1035454;
+		float num2=3.14f;
+		double num3=35.55d;
+		char var1='a';
+		long num4=23423432423L;
+		boolean value=true;
+		String name="hello";
+		System.out.println("byte:"+num);
+		System.out.println("int:"+num1);
+		System.out.println("float:"+num2);
+		System.out.println("double:"+num3);
+		System.out.println("char:"+var1);
+		System.out.println("long:"+num4);
+		System.out.println("boolean:"+value);
+		System.out.println("String:"+name);
+
+	}
+
+}
